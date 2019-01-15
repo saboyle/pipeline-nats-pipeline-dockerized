e491b816d63251319b99496c1240874332b37c5c
Steve Boyle <boylesa@gmail.com>
2019-01-15 18:16:12 +0000

    Added dockerized pipeline Version 1.

---
+++ Version 1 - basic dockerized pipeline/README.md
@@ -0,0 +1,45 @@
+# Version 1
+
+## To run example
+
+* CD to directory
+* Run 3 terminals:
+
+Terminal 1: Runs the Dockerized pipeline
+``` bash
+# To build the pipeline docker image
+docker-compose build
+
+# To run the pipeline & single NATS server
+docker-compose up
+```
+Terminal 2: Runs a wiretap on the output message queue
+``` bash
+python nats-wiretap p1.s3
+```
+
+Terminal 3: Generates test messages and sends to input queue.
+``` bash
+python client.py
+```
+
+The configuration of the Dockerized pipeline is defined in the docker-compose.yml file:
+
+``` yaml
+version: '0.1'
+
+services:
+
+  nats:
+      image: nats:latest
+      ports:
+      - "4222:4222"
+      - "6222:6222"
+      - "8222:8222"
+
+  pipeline:
+    build: .
+    links:
+      - nats
+
+```
--- Version 1 - basic dockerized pipeline/pipeline.py
+++ Version 1 - basic dockerized pipeline/pipeline.py
@@ -15,18 +15,23 @@ def pipeline_p1(loop):
     # Connection to NATS
     #####################
     nc = NATS()
-    yield from nc.connect("localhost:4222", loop=loop)
+    logger.info("Attempting to connect to nats server")
+    yield from nc.connect("nats:4222", loop=loop)
+    logger.info('Attached to nats server')
 
     #####################
     # Message Handlers
     #####################
     async def mh_s1(msg):
+        logger.info('In S1')
         await nc.publish("p1.s1", msg.data)
 
     async def mh_s2(msg):
+        logger.info('In S2')
         await nc.publish("p1.s2", msg.data)
 
     async def mh_s3(msg):
+        logger.info('In S3')
         await nc.publish("p1.s3", msg.data)
 
     ######################
@@ -43,7 +48,7 @@ if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s, %(message)s', level=logging.DEBUG)
     logger = logging.getLogger(__name__)
 
-    print("Constructing test pipeline - P1")
+    logger.info("Constructing test pipeline - P1")
 
     event_loop = asyncio.get_event_loop()
     event_loop.run_until_complete(pipeline_p1(event_loop))
